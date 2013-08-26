385faca36b6fad226647459d76a20c1877ab9691
Ryan Endacott <rzeg24@gmail.com>
2013-08-25 22:34:26 -0500

    Started HashTable and LinkedList.

---
+++ ruby/hash_table.rb
@@ -0,0 +1,27 @@
+
+class HashTable
+
+  def initialize(capacity = 10)
+    @capacity = capacity
+    @array = Array.new(capacity)
+  end
+
+  def [](key)
+    h = hash(key)
+    list = @array[h]
+    list[key]
+  end
+
+  def []=(key, value)
+    h = hash(key)
+    list = @array[h] || LinkedList.new
+    list.insert_or_update(key, value)
+  end
+
+  private
+
+    # Trivial hash algorithm by adding char values
+    def hash(key)
+      key.to_s.chars.map(&:ord).sum % @capacity
+    end
+end
